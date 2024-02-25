ffb3053a474edde00277789e075e9c9c270da2b3
OWAIS SHAIKH <157890488+hafiz-owais-sheikh@users.noreply.github.com>
2024-02-26 00:56:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,108 @@
+function assignment() {
+    // 4 table
+
+    
+document.write("4 table");
+document.body.appendChild(document.createElement("br"));
+
+
+  var number = 4;
+  for (let index = 1; index <= 10; index++) {
+    document.write(4);
+    document.write(" X ");
+    document.write(index);
+    document.write(" = ");
+    document.write(4 * index);
+    document.body.appendChild(document.createElement("br"));
+  }
+  document.body.appendChild(document.createElement("br"));
+
+//   add two number
+document.write("add two number");
+document.body.appendChild(document.createElement("br"));
+
+
+
+var number1 = 4
+var number2 = 5
+
+
+document.write('Sum of ');
+
+document.write(number1);
+document.write(" and ");
+document.write(number2);
+document.write(" is ");
+document.write(number1+ number2);
+document.body.appendChild(document.createElement("br"));
+document.write("sub two number");
+document.body.appendChild(document.createElement("br"));
+
+
+
+var number1 = 4
+var number2 = 5
+
+
+document.write('difference of ');
+
+document.write(number1);
+document.write(" and ");
+document.write(number2);
+document.write(" is ");
+document.write(number1- number2);
+document.body.appendChild(document.createElement("br"));
+document.write("multiplication two number");
+document.body.appendChild(document.createElement("br"));
+
+
+
+var number1 = 4
+var number2 = 5
+
+
+document.write('multiplication of ');
+
+document.write(number1);
+document.write(" and ");
+document.write(number2);
+document.write(" is ");
+document.write(number1* number2);
+document.body.appendChild(document.createElement("br"));
+document.write("division two number");
+document.body.appendChild(document.createElement("br"));
+
+
+
+var number1 = 4
+var number2 = 5
+
+
+document.write('division of ');
+
+document.write(number1);
+document.write(" and ");
+document.write(number2);
+document.write(" is ");
+document.write(number1/ number2);
+
+document.body.appendChild(document.createElement("br"));
+document.write("modulus two number");
+document.body.appendChild(document.createElement("br"));
+
+
+
+var number1 = 20
+var number2 = 3
+
+document.write('modulus of ');
+
+document.write(number1);
+document.write(" and ");
+document.write(number2);
+document.write(" is ");
+document.write(number1% number2);
+
+}
+
+window.onload = assignment;
